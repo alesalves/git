9b32d2a068357c2f77991ef2fa17bcfa1c33e638
alesalves@hotmail.com <alfa01>
2021-02-14 17:04:31 -0300

    sdf

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
   echo "Olá raiMundo no Mato!";
-  echo " muita doideora";
+  echo " muita doideora afdafaçld çaldfça ldfçlakdç lkadç flkasfdç";
   echo "<html><h1>TELEVISAO NA SALA e no quarto  yuyutyu tyt</h1></HTML>";
 ?>
